fd3b06335c1a80c3fe8d234285662cbd722fad9c
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-12-18 17:03:29 +0100

    fix 1-3

--- 0001_screen.py
+++ 0001_screen.py
@@ -2,21 +2,20 @@ import pygame
 
 pygame.init()
 
-screen_size = (640, 480)
+screenSize = (640, 480)
 WHITE = (255, 255, 255)
 
-screen = pygame.display.set_mode(screen_size)
+screen = pygame.display.set_mode(screenSize)
 pygame.display.set_caption("A Simple Window")
 
 done = False
-
-# per il momento qui!
-screen.fill(WHITE)
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+    screen.fill(WHITE)
 
     pygame.display.flip()
 
--- 0002_draw.py
+++ 0002_draw.py
@@ -2,28 +2,15 @@ import pygame
 
 pygame.init()
 
-screen_size = (640, 480)
+screenSize = (640, 480)
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
-screen = pygame.display.set_mode(screen_size)
+screen = pygame.display.set_mode(screenSize)
 pygame.display.set_caption("Drawing ...")
 
-screen.fill(WHITE)
-
-# draw some rect (x, y, width, height)
-
-# top left
-pygame.draw.rect(screen, RED, (20, 20, 100, 20))
-
-# top right
-pygame.draw.rect(screen, RED, [screen.get_rect().width-20-100, 20, 100, 20])
-
-# draw some lines
-for i in range(10):
-    pygame.draw.line(screen, GREEN, (20+i*10, 100+i*30), (620-i*10, 100+i*30))
 
 done = False
 
@@ -31,7 +18,17 @@ while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             done = True
-    
+
+    screen.fill(WHITE)
+
+    # draw some lines
+    for i in range(10):
+        pygame.draw.line(screen, GREEN, (20+i*10, 100+i*30), (620-i*10, 100+i*30))
+
+    # draw some rect(screen, Color, (x, y, width, height))
+    pygame.draw.rect(screen, RED, (20, 20, 100, 20))
+    pygame.draw.rect(screen, RED, [screen.get_rect().width-20-100, 20, 100, 20])
+
     pygame.display.flip()
 
 pygame.quit()
